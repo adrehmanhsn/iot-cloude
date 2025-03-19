310a1b0ee1a8552b0411afb8779edcbbc78f7cc7
adrehman <adrehmanhsn@hotmail.com>
2025-03-19 14:22:03 +0200

    Adding The Commands Interfaces And command Controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function getPendingTasks()
+    {
+        $tasks = Task::where('status', 'pending')->first();
+
+        return response()->json($tasks);
+    }
+
+    public function UpdateTaskStatus(Request $request, $Task_id)
+    {
+        $task = Task::find($Task_id);
+        if ($task) {
+            $task->status = $request->status;
+            $task->save();
+        }
+        return response()->json($task);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\SensorController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
 Route::post('/devices/{device}/sensors', [SensorController::class, 'store']);
+
+Route::get('/tasks/pending', [TaskController::class, 'getPendingTasks']);
+Route::patch('/tasks/{Task_id}', [TaskController::class, 'UpdateTaskStatus']);
